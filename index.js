85fd22057391f922421f88f532eed6b70d590b95
Luis Manuel Noguerol Ramirez <luismanuelchazy@gmail.com>
2019-10-14 17:27:09 +0200

    return promise until file are loaded

--- index.js
+++ index.js
@@ -1,27 +1,29 @@
 const loadCsv = (e) => {
   const file = e.target.files[0]
   const reader = new FileReader()
-  const result = {}
-  reader.onload = (f) => {
-    const rows = f.target.result.split('\n')
-    const headers = rows[0].split(',')
-    for (let h = 0; h < headers.length; h++) {
-      result[headers[h]] = []
-    }
-    for (let v = 1; v < rows.length - 1; v++) {
-      const values = rows[v].split(',')
-      for (let i = 0; i < values.length; i++) {
-        const val = +values[i]
-        if (!isNaN(val)) {
-          result[headers[i]].push(val)
-        } else {
-          result[headers[i]] = false
+  return new Promise(resolve => {
+    const result = {}
+    reader.onload = (f) => {
+      const rows = f.target.result.split('\n')
+      const headers = rows[0].split(',')
+      for (let h = 0; h < headers.length; h++) {
+        result[headers[h]] = []
+      }
+      for (let v = 1; v < rows.length - 1; v++) {
+        const values = rows[v].split(',')
+        for (let i = 0; i < values.length; i++) {
+          const val = +values[i]
+          if (!isNaN(val)) {
+            result[headers[i]].push(val)
+          } else {
+            result[headers[i]] = false
+          }
         }
       }
+      resolve(result)
     }
-    return result
-  }
-  reader.readAsBinaryString(file)
+    reader.readAsBinaryString(file)
+  })
 }
 
 export default loadCsv
